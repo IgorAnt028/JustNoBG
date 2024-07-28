aaf9532fb20f16b109180682a0417ce043f3d255
IgorAnt028 <118114530+IgorAnt028@users.noreply.github.com>
2024-07-28 12:26:02 +0300

    The "Next" button is now working
    Next button could cause errors

--- JustNoBG.py
+++ JustNoBG.py
@@ -29,16 +29,17 @@ class Window:
         btn.place(x=410, y=10)
 
         self.btn_next = tkinter.Button(text="Next", command=self.next_image)
-        self.btn_next.place(x=410, y=155)
+        self.btn_next.place(x=-100, y=-100)
 
         self.window.bind('<Button-1>', self.create_point)
 
         self.window.mainloop()
 
-    def new_image(self, url=None):
+    def update_image(self, url=None):
         if url is not None:
             pass
 
+        # image resize
         if self.img.size[0] > self.img.size[1]:
             self.img = self.img.resize((400, round(self.img.size[1] / (self.img.size[0] / 400))))
         else:
@@ -53,14 +54,15 @@ class Window:
         self.window.update()
 
     def next_image(self, ):
+        # Open the next image
         self.index += 1
         self.img = Image.open(self.imgs[self.index]).convert('RGBA')
         self.size = [self.img.size[0], self.img.size[1]]
 
-        self.new_image()
+        self.update_image()
 
         if self.index + 1 >= len(self.imgs):
-            self.btn_next.destroy()
+            self.btn_next.place(x=-100, y=-100)
 
         self.window.update()
 
@@ -72,23 +74,29 @@ class Window:
             self.canvas.create_oval(x - 2, y - 2, x + 2, y + 2, fill="red")
 
     def open_files(self, ):
+        # files open
         self.imgs = fd.askopenfilenames()
+        self.index = 0
 
+        # Place the new image
         if len(self.imgs) > 0:
             if len(self.imgs) > 1:
-                self.btn_next = tkinter.Button(text="Next", command=self.next_image)
                 self.btn_next.place(x=410, y=155)
 
             self.img = Image.open(self.imgs[0]).convert('RGBA')
 
+            # image resize
             if self.img.size[0] > self.img.size[1]:
                 self.img = self.img.resize((400, math.ceil(self.img.size[1] / (self.img.size[0] / 400))))
             else:
                 self.img = self.img.resize((math.ceil(self.img.size[0] / (self.img.size[1] / 400)), 400))
 
-            self.new_image()
+            self.update_image()
 
     def remove_button(self, ):
+        # save old image
+        self.img.save("old_" + re.sub(r"^.*/", "", self.imgs[self.index]))
+
         # Green circle
         self.canvas.create_oval(460, 130, 470, 140, fill="green")
 
@@ -141,10 +149,11 @@ class Window:
                 if pixels[x, y] != (0, 0, 0, 0):
                     pos[3] = y + 1
 
+        # save image
         self.img = self.img.crop((pos[0], pos[1], pos[2], pos[3]))
         self.img.resize(([self.img.size[0], self.img.size[1]]))
-        self.img.save("new_" + re.sub(r"^.*/", "", self.imgs[self.index]))
-        self.new_image()
+        self.img.save(re.sub(r"^.*/", "", self.imgs[self.index]))
+        self.update_image()
 
 
 window = Window()
