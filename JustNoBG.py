d80487f85876f1eb54967c60dba6e7e747fa008e
IgorAnt028 <118114530+IgorAnt028@users.noreply.github.com>
2024-07-28 13:32:22 +0300

    "Non-interface" mode
    Was added a possibility remove BG without interface

--- JustNoBG.py
+++ JustNoBG.py
@@ -156,4 +156,68 @@ class Window:
         self.update_image()
 
 
-window = Window()
+if len(sys.argv) > 1:
+    for url in sys.argv[1:]:
+        img = Image.open(url).convert('RGBA')
+
+        # save old image
+        img.save("old_" + re.sub(r"^.*\\", "", url))
+
+        # BG removing
+        rep_value = (0, 0, 0, 0)
+        seed = (0, 0)
+        ImageDraw.floodfill(img, seed, rep_value, thresh=100)
+
+        seed = (img.size[0] - 1, 0)
+        ImageDraw.floodfill(img, seed, rep_value, thresh=100)
+
+        seed = (0, img.size[1] - 1)
+        ImageDraw.floodfill(img, seed, rep_value, thresh=100)
+
+        seed = (img.size[0] - 1, img.size[1] - 1)
+        ImageDraw.floodfill(img, seed, rep_value, thresh=100)
+
+        # Crop
+        pixels = img.load()
+        pos = [0, 0, 0, 0]
+
+        # X
+        for x in range(img.size[0]):
+            if pos[0] != 0:
+                break
+
+            for y in range(img.size[1]):
+                if pixels[x, y] != (0, 0, 0, 0):
+                    pos[0] = x - 1
+
+        # Y
+        for y in range(img.size[1]):
+            if pos[1] != 0:
+                break
+
+            for x in range(img.size[0]):
+                if pixels[x, y] != (0, 0, 0, 0):
+                    pos[1] = y - 1
+
+        # -X
+        for x in range(img.size[0] - 1, 1, -1):
+            if pos[2] != 0:
+                break
+
+            for y in range(img.size[1] - 1, 1, -1):
+                if pixels[x, y] != (0, 0, 0, 0):
+                    pos[2] = x + 1
+
+        # -Y
+        for y in range(img.size[1] - 1, 1, -1):
+            if pos[3] != 0:
+                break
+
+            for x in range(img.size[0] - 1, 1, -1):
+                if pixels[x, y] != (0, 0, 0, 0):
+                    pos[3] = y + 1
+
+        # save image
+        img.crop((pos[0], pos[1], pos[2], pos[3])).save(re.sub(r"^.*\\", "", url))
+else:
+    window = Window()
